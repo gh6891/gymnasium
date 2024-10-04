57b430ffabc6ab72e0f52109425c0e9e7830ca15
Gunhee Nam <gh000604@gmail.com>
2024-10-04 16:40:32 +0900

    second_push

--- Pendulum/my_version.py
+++ Pendulum/my_version.py
@@ -18,12 +18,12 @@ is_ipython = 'inline' in matplotlib.get_backend()
 if is_ipython:
     from IPython import display
 
-env = gym.make('Pendulum-v1', g=9.81)
-env = gym.wrappers.TimeLimit(env, max_episode_steps=1000)
+# env = gym.make('Pendulum-v1', g=9.81)
+# env = gym.wrappers.TimeLimit(env, max_episode_steps=1000)
 
-# GPU를 사용할 경우
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(device)
+# # GPU를 사용할 경우
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# print(device)
 
 Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
 
@@ -57,165 +57,143 @@ class DQN(nn.Module):
         x = F.relu(self.layer1(x))
         x = F.relu(self.layer2(x))
         return self.layer3(x)
-    
-def select_action(state):
-    #select_action but
-    global steps_done
-    sample = random.random()
-    eps_threshold = EPS_END + (EPS_START - EPS_END) * math.exp(-1. * steps_done / EPS_DECAY)
-    steps_done = steps_done + 1
 
-    if sample > eps_threshold:
-        with torch.no_grad():
-            # print("action : ", policy_net(state))
-            return policy_net(state)
-    else:
-        # print("random : ", torch.tensor([env.action_space.sample()], device=device, dtype = torch.float32))
-        return torch.tensor([env.action_space.sample()], device=device, dtype = torch.float32)
-    
-def plot_durations(show_result=False):
-    plt.figure(1)
-    rewards_t = torch.tensor(episode_rewards, dtype = torch.float)
-    durations_t = torch.tensor(episode_durations, dtype=torch.float)
-    if show_result:
-        plt.title('Result')
-    else:
-        plt.clf()
-        plt.title('Training...')
-    plt.xlabel('Episode')
-    plt.ylabel('reward')
-    plt.plot(rewards_t.numpy())
-    # 100개의 에피소드 평균을 가져 와서 도표 그리기
-    if len(rewards_t) >= 100:
-        means = rewards_t.unfold(0, 100, 1).mean(1).view(-1)
-        means = torch.cat((torch.zeros(99), means))
-        plt.plot(means.numpy())
+class PendulumAgent():
+    def __init__(self, env, n_observations, n_actions):
+        self.env = env
+        self.n_observations = n_observations
+        self.n_actions = n_actions
 
-    plt.pause(0.001)  # 도표가 업데이트되도록 잠시 멈춤
-    if is_ipython:
-        if not show_result:
-            display.display(plt.gcf())
-            display.clear_output(wait=True)
+        # 하이퍼파라미터
+        self.BATCH_SIZE = 128
+        self.GAMMA = 0.99
+        self.TAU = 0.005
+        self.LR = 1e-4
+
+        # 네트워크 및 메모리 초기화
+        self.policy_net = DQN(n_observations, n_actions).to(device)
+        self.target_net = DQN(n_observations, n_actions).to(device)
+        self.target_net.load_state_dict(self.policy_net.state_dict())
+        self.optimizer = optim.AdamW(self.policy_net.parameters(), lr=self.LR, amsgrad=True)
+        self.memory = ReplayMemory(100000)
+
+        self.steps_done = 0
+        self.EPS_END = 0.05
+        self.EPS_START = 0.9
+        self.EPS_DECAY = 10000
+
+        # 학습 기록
+        self.episode_durations = []
+        self.episode_rewards = []
+        self.cumulative_reward = 0
+
+    def select_action(self, state):
+        sample = random.random()
+        eps_threshold = self.EPS_END + (self.EPS_START - self.EPS_END) * math.exp(-1. * self.steps_done / self.EPS_DECAY)
+        self.steps_done += 1
+
+        if sample > eps_threshold:
+            with torch.no_grad():
+                return self.policy_net(state)
         else:
-            display.display(plt.gcf())
+            return torch.tensor([self.env.action_space.sample()], device=device, dtype=torch.float32)
+        
+    def learning(self, num_episodes):
+        for i_episode in range(num_episodes):
+            state, info = self.env.reset()
+            state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
+            for t in count():
+                action = self.select_action(state)
+                observation, reward, terminated, truncated, _ = self.env.step([action.item()])
+                reward = torch.tensor([reward], device=device)
+                done = terminated or truncated
+                self.cumulative_reward += reward.item()
 
-def optimize_model():
-    if len(memory) < BATCH_SIZE:
-        print("memory's len needs more size")
-        return
-    transitions = memory.sample(BATCH_SIZE)
-    # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
-    # detailed explanation). 이것은 batch-array의 Transitions을 Transition의 batch-arrays로
-    # 전환합니다.
-    batch = Transition(*zip(*transitions))
+                if terminated:
+                    next_state = None
+                else:
+                    next_state = torch.tensor(observation, dtype=torch.float32, device=device).unsqueeze(0)
 
-    # 최종이 아닌 상태의 마스크를 계산하고 배치 요소를 연결합니다
-    # (최종 상태는 시뮬레이션이 종료 된 이후의 상태)
-    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)), device=device, dtype=torch.bool)
-    non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
+                self.memory.push(state, action, next_state, reward)
+                state = next_state
+                self.optimize_model()
 
-    state_batch = torch.cat(batch.state)
-    action_batch = torch.cat(batch.action)
-    reward_batch = torch.cat(batch.reward)
+                # Soft update for target network
+                target_net_state_dict = self.target_net.state_dict()
+                policy_net_state_dict = self.policy_net.state_dict()
+                
+                for key in policy_net_state_dict:
+                    target_net_state_dict[key] = policy_net_state_dict[key] * self.TAU + target_net_state_dict[key] * (1 - self.TAU)
+                self.target_net.load_state_dict(target_net_state_dict)
+                if done:
+                    self.episode_rewards.append(self.cumulative_reward)
+                    print("episode : ", i_episode, "reward : ", self.cumulative_reward, "mean : ")
+                    self.cumulative_reward = 0
+                    self.episode_durations.append(t + 1)
+                    break
 
-    # Q(s_t, a) 계산 - 모델이 Q(s_t)를 계산하고, 취한 행동의 열을 선택합니다.
-    # 이들은 policy_net에 따라 각 배치 상태에 대해 선택된 행동입니다.
-    # print(policy_net(state_batch))
-    # print("policy_net: ", policy_net(state_batch))
-    # print("gather: ", policy_net(state_batch).gather(1, action_batch))
-    state_action_values = policy_net(state_batch)
-
-    # print(state_action_values)
-    # 모든 다음 상태를 위한 V(s_{t+1}) 계산
-    # non_final_next_states의 행동들에 대한 기대값은 "이전" target_net을 기반으로 계산됩니다.
-    # max(1).values로 최고의 보상을 선택하십시오.
-    # 이것은 마스크를 기반으로 병합되어 기대 상태 값을 갖거나 상태가 최종인 경우 0을 갖습니다.
-    next_state_values = torch.zeros(BATCH_SIZE, device=device)
-    with torch.no_grad():
-        next_state_values[non_final_mask] = target_net(non_final_next_states).max(1).values
-    # 기대 Q 값 계산
-    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
-    # Huber 손실 계산
-    criterion = nn.SmoothL1Loss()
-    loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
-
-    # 모델 최적화
-    optimizer.zero_grad()
-    loss.backward()
-    # 변화도 클리핑 바꿔치기
-    torch.nn.utils.clip_grad_value_(policy_net.parameters(), 100)
-    optimizer.step()
-
-def learning():
-    for i_episode in range(num_episodes):
-        # 환경과 상태 초기화
-        state, info = env.reset()
-        state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
-        for t in count():
-            action = select_action(state)
-            observation, reward, terminated, truncated, _ = env.step([action.item()])
-            reward = torch.tensor([reward], device=device)
-            done = terminated or truncated
-            cumulative_reward += reward.item()
-            if terminated:
-                next_state = None
-            else:
-                next_state = torch.tensor(observation, dtype=torch.float32, device=device).unsqueeze(0)
-
-            memory.push(state, action, next_state, reward)
-
-            state = next_state
-            optimize_model()
-
-            target_net_state_dict = target_net.state_dict()
-            policy_net_state_dict = policy_net.state_dict()
-            for key in policy_net_state_dict:
-                target_net_state_dict[key] = policy_net_state_dict[key]*TAU + target_net_state_dict[key]*(1-TAU)
-            target_net.load_state_dict(target_net_state_dict)
-
-            if done:
-                episode_rewards.append(cumulative_reward)
-                cumulative_reward = 0
-                episode_durations.append(t + 1)
-                plot_durations()
-                break
-    torch.save(policy_net.state_dict(), 'final_pendulum_policy_net.pth')
-    print('Complete')
-    plot_durations(show_result=True)
-    plt.ioff()
-    plt.show()
     
-BATCH_SIZE = 128
-GAMMA = 0.99
-TAU = 0.005
-LR = 1e-4
-n_actions = env.action_space.shape[0]
-n_observations = env.observation_space.shape[0]
+    def plot_durations(self, show_result=False):
+        plt.figure(1)
+        rewards_t = torch.tensor(self.episode_rewards, dtype = torch.float)
+        durations_t = torch.tensor(self.episode_durations, dtype=torch.float)
+        if show_result:
+            plt.title('Result')
+        else:
+            plt.clf()
+            plt.title('Training...')
+        plt.xlabel('Episode')
+        plt.ylabel('reward')
+        plt.plot(rewards_t.numpy())
+        # 100개의 에피소드 평균을 가져 와서 도표 그리기
+        if len(rewards_t) >= 100:
+            means = rewards_t.unfold(0, 100, 1).mean(1).view(-1)
+            means = torch.cat((torch.zeros(99), means))
+            plt.plot(means.numpy())
+
+        plt.pause(0.001)  # 도표가 업데이트되도록 잠시 멈춤
+        if is_ipython:
+            if not show_result:
+                display.display(plt.gcf())
+                display.clear_output(wait=True)
+            else:
+                display.display(plt.gcf())
+
+    def optimize_model(self):
+        if len(self.memory) < self.BATCH_SIZE:
+            print("memory's len needs more size")
+            return
+        transitions = self.memory.sample(self.BATCH_SIZE)
+        batch = Transition(*zip(*transitions))
+        non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)), device=device, dtype=torch.bool)
+        non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
+
+        state_batch = torch.cat(batch.state)
+        # action_batch = torch.cat(batch.action)
+        reward_batch = torch.cat(batch.reward)
+
+        state_action_values = self.policy_net(state_batch)
+        next_state_values = torch.zeros(self.BATCH_SIZE, device=device)
+        with torch.no_grad():
+            next_state_values[non_final_mask] = self.target_net(non_final_next_states).max(1).values
+        # 기대 Q 값 계산
+        expected_state_action_values = (next_state_values * self.GAMMA) + reward_batch
+        # Huber 손실 계산
+        criterion = nn.SmoothL1Loss()
+        loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
 
 
-policy_net = DQN(n_observations, n_actions).to(device)
-target_net = DQN(n_observations, n_actions).to(device)
-target_net.load_state_dict(policy_net.state_dict())
-
-optimizer = optim.AdamW(policy_net.parameters(), lr=LR, amsgrad=True)
-memory = ReplayMemory(100000)
-
-step_done = 0
-EPS_END = 0.05
-EPS_START = 0.9
-EPS_DECAY = 10000
-steps_done = 0
-
-num_episodes = 2000
-episode_durations = []
-cumulative_reward = 0
-episode_rewards = []
-
+        # 모델 최적화
+        self.optimizer.zero_grad()
+        loss.backward()
+        # 변화도 클리핑 바꿔치기
+        torch.nn.utils.clip_grad_value_(self.policy_net.parameters(), 100)
+        self.optimizer.step()
 
 def control():
     env = gym.make('Pendulum-v1', g=9.81, render_mode = 'rgb_array')
     env = gym.wrappers.TimeLimit(env, max_episode_steps=1000)
-    env = RecordVideo(env, 'pendulum_video', episode_trigger=lambda episode_number: True)
+    # env = RecordVideo(env, 'pendulum_video', episode_trigger=lambda episode_number: True)
     n_actions = env.action_space.shape[0]
     n_observations = env.observation_space.shape[0]
 
@@ -243,5 +221,15 @@ def control():
     env.close()
 
 if __name__ == "__main__":
-    learning()
-    # control()
+    env = gym.make('Pendulum-v1', g=9.81)
+    env = gym.wrappers.TimeLimit(env, max_episode_steps=1000)
+
+    # GPU를 사용할 경우
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
+
+    n_actions = env.action_space.shape[0]
+    n_observations = env.observation_space.shape[0]
+
+    agent = PendulumAgent(env, n_observations, n_actions)
+    agent.learning(num_episodes=500)
